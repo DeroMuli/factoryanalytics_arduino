41be60e847fbb6f7327718c3781dcaca722e4f37
DeroMuli <musembimuli1999@gmail.com>
2023-02-16 11:46:51 +0300

    connected to wifi and added a file to hide credentials

--- factoryanalytics_arduino.ino
+++ factoryanalytics_arduino.ino
@@ -1,6 +1,10 @@
+#include <ESP8266WiFi.h>
+#include "secret_file.h"
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(D4,OUTPUT);
+  connectToWiFi();
 }
 
 void loop() {
@@ -9,4 +13,32 @@ void loop() {
   delay(1000);
   digitalWrite(D4,LOW);
   delay(1000);
+}
+
+/*
+* Connect your controller to WiFi
+*/
+void connectToWiFi() {
+//Connect to WiFi Network
+   Serial.begin(9600);
+   Serial.println();
+   Serial.println();
+   Serial.print("Connecting to WiFi");
+   Serial.println("...");
+   WiFi.begin(getWifiSsid(), getWifiPassword());
+   int retries = 0;
+while ((WiFi.status() != WL_CONNECTED) && (retries < 15)) {
+   retries++;
+   delay(500);
+   Serial.print(".");
+}
+if (retries > 14) {
+    Serial.println(F("WiFi connection FAILED"));
+}
+if (WiFi.status() == WL_CONNECTED) {
+    Serial.println(F("WiFi connected!"));
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+    Serial.println(F("Setup ready"));
 }
---
+++ secret_file.h
@@ -0,0 +1,9 @@
+#ifndef SECRET_FILE_H
+#define SECRET_FILE_H
+
+#include <Arduino.h>
+
+const char* getWifiSsid();
+const char* getWifiPassword();
+
+#endif // SECRET_FILE_H
