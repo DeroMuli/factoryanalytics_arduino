4f5dfd0f8e6769b4e507c1ecbb3de48e7f017926
DeroMuli <musembimuli1999@gmail.com>
2023-02-17 11:26:08 +0300

    built a simple webseerver

--- factoryanalytics_arduino.ino
+++ factoryanalytics_arduino.ino
@@ -1,5 +1,12 @@
 #include <ESP8266WiFi.h>
 #include "secret_file.h"
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebSrv.h>
+#include <AsyncWebSocket.h>
+#include <ArduinoJson.h>
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
 void setup() {
   // put your setup code here, to run once:
@@ -41,4 +48,35 @@ if (WiFi.status() == WL_CONNECTED) {
     Serial.println(WiFi.localIP());
 }
     Serial.println(F("Setup ready"));
+      // Start the WebSocket server
+    // ws.onEvent(onWebSocketEvent);
+    // server.addHandler(&ws);
+      // Start the HTTP server
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      StaticJsonDocument<200> doc;
+      doc["name"] = "John Doe";
+      doc["age"] = 42;
+      String jsonStr;
+      serializeJson(doc, jsonStr);
+      request->send(200,"application/json", jsonStr);
+    });
+    server.begin();
+     //Serial.println("HTTP server started");
+}
+
+void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  if(type == WS_EVT_CONNECT) {
+    Serial.printf("Websocket client #%u connected\n", client->id());
+  } else if(type == WS_EVT_DISCONNECT) {
+    Serial.printf("Websocket client #%u disconnected\n", client->id());
+  } else if(type == WS_EVT_DATA) {
+    Serial.printf("Websocket client #%u data received: ", client->id());
+    for(size_t i=0; i<len; i++) {
+      Serial.print((char) data[i]);
+    }
+    Serial.println();
+  }
+  else if(type == WS_EVT_ERROR){
+    Serial.println("internal error");
+  }
 }
