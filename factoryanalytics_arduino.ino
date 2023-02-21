c97df6531ae92ba849cb7e01728e65c71c15f751
DeroMuli <musembimuli1999@gmail.com>
2023-02-21 16:04:56 +0300

    added temp sensor

--- factoryanalytics_arduino.ino
+++ factoryanalytics_arduino.ino
@@ -3,19 +3,42 @@
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebSrv.h>
 #include <ArduinoJson.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 int pinState = LOW;
+OneWire oneWire(D5);
+DallasTemperature sensors(&oneWire);
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(D4,OUTPUT);
   connectToWiFi();
+  sensors.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // call sensors.requestTemperatures() to issue a global temperature
+  // request to all devices on the bus
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+  // After we got the temperatures, we can print them here.
+  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+  float tempC = sensors.getTempCByIndex(0);
+
+  // Check if reading was successful
+  if (tempC != DEVICE_DISCONNECTED_C)
+  {
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+    Serial.println(tempC);
+  }
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+  }
   StaticJsonDocument<200> jsonDoc;
   jsonDoc["temp"] = 34;
   jsonDoc["speed"] = 125;
